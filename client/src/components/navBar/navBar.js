99f3191ba9c57a5404ea8e0d58673a3372477b48
mohammedali988 <mohd.os.1998@gmail.com>
2020-07-13 16:51:10 +0300

    newComponent

---
+++ client/src/components/navBar/navBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class bar extends React.Component{
+    render(){
+        return (
+            <div>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="log in">log in</a></li>
+                    <li><a href="sign up">sign up</a></li>
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
+export default bar;
