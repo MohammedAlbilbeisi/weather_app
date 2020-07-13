59b8d4245b67eb60785815f1f5b8492dd94271a1
naderaqaoud20 <naderaqaoud2000@gmail.com>
2020-07-13 20:27:56 +0300

    sign in and login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+<<<<<<< Updated upstream
 
+=======
+import Weathers from "./components/weather-react"
+import Login from "./components/log_in"
+import SignUp from "./components/sign_up"
+>>>>>>> Stashed changes
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <h1>Peanut butter and jelly</h1>
+=======
+      <Weathers />
+>>>>>>> Stashed changes
     </div>
   );
 }
