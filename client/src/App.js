bbacf871907b9e849e8e1c2009f9357fc8719b3b
naderaqaoud20 <naderaqaoud2000@gmail.com>
2020-07-15 21:08:57 +0300

    final version for sing in

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-import Weathers from "./components/weather-react"
-import Login from "./components/log_in"
 import SignUp from "./components/sign_up"
-import Bar from './component/navBar/navBar';
-import List from './component/list/list';
+import Login from "./components/log_in"
+import {BrowserRouter} from "react-router-dom";  
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Bar/>
-      <Weathers />
-      <List/>
+      <BrowserRouter><SignUp /></BrowserRouter>
     </div>
   );
 }
